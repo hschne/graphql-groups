8c644411b6ac76409b49b4f0570e94252a8eb890
Hans-Jörg Schnedlitz <hans.schnedlitz@gmail.com>
2020-06-23 09:07:22 +0200

    Make aggregates work again

--- lib/graphql/groups/query_executor.rb
+++ lib/graphql/groups/query_executor.rb
@@ -31,36 +31,21 @@ module GraphQL
       end
 
       def transform_result(key, result)
-        # We use the composite key of the query that was run (e.g. [:section_id, :created_at]) as key. The result is the
-        # result set of the corresponding query. We transform this into a nested hash of the form:
-        #
-        #  {
-        #    section_id: {
-        #      <section_id> => {
-        #        nested: {
-        #          created_at: {
-        #            '2020-01-01': {
-        #               count: <value>
-        #            }
-        #          }
-        #        }
-        #      }
-        #    }
-        #  }
+        result.each_with_object({}) do |(aggregate_key, value), object|
+          object.deep_merge!(transform_aggregate(key, aggregate_key, value))
+        end
+      end
+
+      def transform_aggregate(key, aggregate, result)
         result.each_with_object({}) do |(keys, value), object|
-          key    = Array.wrap(key)
-          keys   = Array.wrap(keys)
+          key = Array.wrap(key)
+          keys = Array.wrap(keys)
           nested = [:nested] * (key.length - 1)
-          # We construct an array that will later be turned into the hash. To created the structure we want we zip the key
-          # (e.g. [:section_id, :created_at]) with the result set key (e.g. [1, '2020-01-01']) and then zip that with [:nested]
-          # key resulting in e.g. [:section_id, 1, :nested, :created_at, '2020-01-01'].
-          #
-          # The innermost hash contains the result value of a result row. Merging multiple such hashes together we get a hash
-          # representation of the result of the entire grouping query.
-          #
+
           # See https://stackoverflow.com/a/5095149/2553104
           with_zipped = key.zip(keys).zip(nested).flatten!.compact
-          hash        = with_zipped.reverse.inject(value) { |a, n| { n => a } }
+          with_zipped.append(aggregate)
+          hash = with_zipped.reverse.inject(value) { |a, n| { n => a } }
           object.deep_merge!(hash)
         end
       end
